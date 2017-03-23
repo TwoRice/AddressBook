e68541ea84199f6e79f5f586e294f4b64035ec4d
TwoRice <2rimagowan@gmail.com>
2017-03-23 21:38:16 +0000

    submit button passes if user is adding or editing and for which type of entry through post

--- webapp/edit.php
+++ webapp/edit.php
@@ -6,7 +6,7 @@
 	        <div class="col-xs-12">
 	            
 	            <!--FORM-->
-	            <form method="POST" action="">
+	            <form method="POST" action="php/processEditForm.php">
             		
             		<!--PANEL-->
             		<div class="panel panel-default">
@@ -21,14 +21,14 @@
             				    <?php 
                                     session_start();
                                     $result = $_SESSION['result'];
-                                    foreach($result as $row){
+                                    $info = $result[0];
                                 ?>
 
                                         <!--NAME-->
             				    <div class="row">
             				    	<div class="col-xs-12">
             				    		<label class="control-label" for="name">Name</label>
-            				    		<input type="text" name="name" class="form-control" required="required" value="<?= $row[1] ?>">
+            				    		<input type="text" name="name" class="form-control" required="required" value="<?= $info[1] ?>">
             				    	</div>
             				    </div>
 
@@ -36,27 +36,27 @@
             				    <div class="row">
             				    	<div class="col-xs-12">
             				    	    <label class="control-label" for="address1">Address Line 1</label>
-            				    	    <input type="text" name="address1" class="form-control" value="<?= $row[2] ?>">	
+            				    	    <input type="text" name="address1" class="form-control" value="<?= $info[2] ?>">	
             				    	</div>
             				    </div>
             				    <div class="row">
             				    	<div class="col-xs-12">
             				    	    <label class="control-label" for="address2">Address Line 2</label>
-            				    	    <input type="text" name="address2" class="form-control" value="<?= $row[3] ?>">	
+            				    	    <input type="text" name="address2" class="form-control" value="<?= $info[3] ?>">	
             				    	</div>	
             				    </div>
             				    <div class="row">
             				    	<div class="col-sm-6">
             				    	    <label class="control-label" for="city">City</label>
-            				    	    <input type="text" name="city" class="form-control" value="<?= $row[4] ?>">	
+            				    	    <input type="text" name="city" class="form-control" value="<?= $info[4] ?>">	
             				    	</div>
             				    	<div class="col-sm-4">
             				    		<label class="control-label" for="county">County/State</label>
-            				    	    <input type="text" name="county" class="form-control" value="<?= $row[5] ?>">
+            				    	    <input type="text" name="county" class="form-control" value="<?= $info[5] ?>">
             				    	</div>
             				    	<div class="col-sm-2">
             				    	    <label class="control-label" for="postcode">Postcode</label>
-            				    	    <input type="text" name="postcode" class="form-control" value="<?= $row[6] ?>">	
+            				    	    <input type="text" name="postcode" class="form-control" value="<?= $info[6] ?>">	
             				    	</div>	
             				    </div>
 
@@ -64,35 +64,35 @@
             				    <div class="row">
             				    	<div class="col-xs-12">
             				    	    <label class="control-label" for="email">Email Address</label>
-            				    	    <input type="email" name="email" class="form-control" value="<?= $row[7] ?>">	
+            				    	    <input type="email" name="email" class="form-control" value="<?= $info[7] ?>">	
             				    	</div>
             				    </div>
 
             				    <!--PHONE-->
-            				    <?php if(sizeof($row) < 10){ ?>
+            				    <?php if(strcmp($_SESSION['type'], "org") == 0){ ?>
                                 <div class="row">
             				    	<div class="col-xs-12">
-            				    	    <label class="control-label" for="email">Phone No</label>
-            				    	    <input type="text" name="email" class="form-control" value="<?= $row[8] ?>">	
+            				    	    <label class="control-label" for="phone">Phone No</label>
+            				    	    <input type="text" name="phone" class="form-control" value="<?= $info[8] ?>">	
             				    	</div>	
             				    </div>
                                 <?php }else{ ?>
                                 <div class="row">
                                     <div class="col-xs-12">
-                                        <label class="control-label" for="email">Mobile No</label>
-                                        <input type="text" name="email" class="form-control" value="<?= $row[8] ?>">    
+                                        <label class="control-label" for="mobile">Mobile No</label>
+                                        <input type="text" name="mobile" class="form-control" value="<?= $info[8] ?>">    
                                     </div>  
                                 </div>
                                 <div class="row">
                                     <div class="col-xs-12">
-                                        <label class="control-label" for="email">Home No</label>
-                                        <input type="text" name="email" class="form-control" value="<?= $row[9] ?>">    
+                                        <label class="control-label" for="home">Home No</label>
+                                        <input type="text" name="home" class="form-control" value="<?= $info[9] ?>">    
                                     </div>  
                                 </div>
                                 <div class="row">
                                     <div class="col-xs-12">
-                                        <label class="control-label" for="email">Work No</label>
-                                        <input type="text" name="email" class="form-control" value="<?= $row[10] ?>">    
+                                        <label class="control-label" for="work">Work No</label>
+                                        <input type="text" name="work" class="form-control" value="<?= $info[10] ?>">    
                                     </div>  
                                 </div>
 
@@ -100,24 +100,33 @@
                                 <?php require 'php/selectOrganisations.php'; ?>
                                 <div class="row">
                                     <div class="col-xs-12">
-                                        <label class="control-label" for="organisation"></label>
+                                        <label class="control-label" for="organisation">Organisation</label>
                                         <select name="organisation" class="form-control">
-                                            <?php foreach($result as $orgRow){ ?>
-                                            <option value="<?= orgRow[0].': '.orgRow[1] ?>"></option>
-                                            <?php } ?>    
+                                            <?php 
+                                                foreach($resultAll as $row){
+                                                    if($info[11] === $row[0]){ 
+                                            ?>
+                                            <option value="<?= $row[0] ?>" selected><?= $row[0].': '.$row[1] ?></option>
+                                            <php }else{ ?>
+                                            <option value="<?= $row[0] ?>"><?= $row[0].': '.$row[1] ?></option>
+                                            <?php }} ?>    
                                         </select>
                                     </div>
                                 </div>
                                 <?php } ?>
 
             				    <!--SUBMIT-->
-            				    <div class="row">
+            				    <?php 
+                                    $action = "edit";
+                                    if($result == null){
+                                        $action = "add";
+                                    }
+                                ?>
+                                <div class="row">
             				    	<div class="col-xs-12">
-            				    	    </br><input type="submit" class="btn btn-primary">	
+            				    	    </br><button type="submit" class="btn btn-primary" name="save" value="<?= $action.','.$_SESSION['type'] ?>">Submit</button>
             				    	</div>	
             				    </div>
-
-                                <?php } ?>
 
             				</div>
             			</div>
