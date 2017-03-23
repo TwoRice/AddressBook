10e43c2bd2e9ecb79e42ae557fab21b115cd0d47
TwoRice <2rimagowan@gmail.com>
2017-03-23 00:43:40 +0000

    formatting & commenting

--- webapp/edit.php
+++ webapp/edit.php
@@ -4,17 +4,29 @@
 	<div class="container">
 	    <div class="row">
 	        <div class="col-xs-12">
+	            
+	            <!--FORM-->
 	            <form method="POST" action="">
+            		
+            		<!--PANEL-->
             		<div class="panel panel-default">
+            			
+            		    <!--PANEL HEADER-->
             			<div class="panel-heading">New Entry</div>
+            			
+            			<!--PANEL BODY-->
             			<div class="panel-body">
             				<div class="form-group">
+
+            				    <!--NAME-->
             				    <div class="row">
             				    	<div class="col-xs-12">
             				    		<label class="control-label" for="name">Name</label>
             				    		<input type="text" name="name" class="form-control" required="required">
             				    	</div>
             				    </div>
+
+            				    <!--ADDRESS-->
             				    <div class="row">
             				    	<div class="col-xs-12">
             				    	    <label class="control-label" for="address1">Address Line 1</label>
@@ -41,23 +53,30 @@
             				    	    <input type="text" name="postcode" class="form-control">	
             				    	</div>	
             				    </div>
+
+            				    <!--EMAIL-->
             				    <div class="row">
             				    	<div class="col-xs-12">
             				    	    <label class="control-label" for="email">Email Address</label>
             				    	    <input type="email" name="email" class="form-control">	
             				    	</div>
             				    </div>
+
+            				    <!--PHONE-->
             				    <div class="row">
             				    	<div class="col-xs-12">
             				    	    <label class="control-label" for="email">Phone No</label>
             				    	    <input type="text" name="email" class="form-control">	
             				    	</div>	
             				    </div>
+
+            				    <!--SUBMIT-->
             				    <div class="row">
             				    	<div class="col-xs-12">
             				    	    </br><input type="submit" class="btn btn-primary">	
             				    	</div>	
-            				    </div>            				                				                				    	
+            				    </div>
+
             				</div>
             			</div>
             		</div>
