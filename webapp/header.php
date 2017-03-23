230a25c9df5a3816d4b76f285fa75e41fc55aaaa
TwoRice <2rimagowan@gmail.com>
2017-03-23 19:22:16 +0000

    added sqlconnection require_once

--- webapp/header.php
+++ webapp/header.php
@@ -5,6 +5,7 @@
 	<link rel="stylesheet" href="res/styles.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<?php require_once 'php/sqlConnection.php'; ?>
 </head>
 <body>
 	<div class="container">
@@ -14,10 +15,10 @@
 			</div>
 
 			<div class="row">
-				<div class="col-xs-6">
+				<div class="col-sm-6">
 					<a href="/index.php?table=org"><h2>Organisations</h2></a>
 				</div>
-				<div class="col-xs-6">
+				<div class="col-sm-6">
 					<a href="/index.php?table=people"><h2>People</h2></a>
 				</div>
 			</div>
