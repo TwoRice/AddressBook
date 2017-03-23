0da8222e61c5c4f6b78cf7915cf3487600c356a1
TwoRice <2rimagowan@gmail.com>
2017-03-23 20:19:41 +0000

    fixed html closing value attribute at whitespace

--- webapp/edit.php
+++ webapp/edit.php
@@ -28,7 +28,7 @@
             				    <div class="row">
             				    	<div class="col-xs-12">
             				    		<label class="control-label" for="name">Name</label>
-            				    		<input type="text" name="name" class="form-control" required="required" value=<?= $row[1] ?>>
+            				    		<input type="text" name="name" class="form-control" required="required" value="<?= $row[1] ?>">
             				    	</div>
             				    </div>
 
@@ -36,27 +36,27 @@
             				    <div class="row">
             				    	<div class="col-xs-12">
             				    	    <label class="control-label" for="address1">Address Line 1</label>
-            				    	    <input type="text" name="address1" class="form-control" value=<?= $row[2] ?>>	
+            				    	    <input type="text" name="address1" class="form-control" value="<?= $row[2] ?>">	
             				    	</div>
             				    </div>
             				    <div class="row">
             				    	<div class="col-xs-12">
             				    	    <label class="control-label" for="address2">Address Line 2</label>
-            				    	    <input type="text" name="address2" class="form-control" value=<?= $row[3] ?>>	
+            				    	    <input type="text" name="address2" class="form-control" value="<?= $row[3] ?>">	
             				    	</div>	
             				    </div>
             				    <div class="row">
             				    	<div class="col-sm-6">
             				    	    <label class="control-label" for="city">City</label>
-            				    	    <input type="text" name="city" class="form-control" value=<?= $row[4] ?>>	
+            				    	    <input type="text" name="city" class="form-control" value="<?= $row[4] ?>">	
             				    	</div>
             				    	<div class="col-sm-4">
             				    		<label class="control-label" for="county">County/State</label>
-            				    	    <input type="text" name="county" class="form-control" value=<?= $row[5] ?>>
+            				    	    <input type="text" name="county" class="form-control" value="<?= $row[5] ?>">
             				    	</div>
             				    	<div class="col-sm-2">
             				    	    <label class="control-label" for="postcode">Postcode</label>
-            				    	    <input type="text" name="postcode" class="form-control" value=<?= $row[6] ?>>	
+            				    	    <input type="text" name="postcode" class="form-control" value="<?= $row[6] ?>">	
             				    	</div>	
             				    </div>
 
@@ -64,7 +64,7 @@
             				    <div class="row">
             				    	<div class="col-xs-12">
             				    	    <label class="control-label" for="email">Email Address</label>
-            				    	    <input type="email" name="email" class="form-control" value=<?= $row[7] ?>>	
+            				    	    <input type="email" name="email" class="form-control" value="<?= $row[7] ?>">	
             				    	</div>
             				    </div>
 
@@ -72,7 +72,7 @@
             				    <div class="row">
             				    	<div class="col-xs-12">
             				    	    <label class="control-label" for="email">Phone No</label>
-            				    	    <input type="text" name="email" class="form-control" value=<?= $row[8] ?>>	
+            				    	    <input type="text" name="email" class="form-control" value="<?= $row[8] ?>">	
             				    	</div>	
             				    </div>
 
