ebe1b2de6df99a042b93232592a9ca93aa665a06
TwoRice <2rimagowan@gmail.com>
2017-03-23 23:42:02 +0000

    Added create table if not exists statements for if the database is deleted

--- webapp/php/sqlConnection.php
+++ webapp/php/sqlConnection.php
@@ -2,4 +2,9 @@
 	$root = realpath($_SERVER["DOCUMENT_ROOT"]);
 	$connection = new PDO('sqlite:'.$root.'/db/AddressBook.db') or die("connection to database failed");
 	$connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+
+	$ps = $connection->prepare('CREATE TABLE IF NOT EXISTS "Contact" ( `ID` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `name` TEXT NOT NULL, `address1` TEXT, `address2` TEXT, `city` TEXT, `county` TEXT, `postcode` TEXT, `email` TEXT, `mobile` TEXT, `home` TEXT, `work` TEXT, `organisationID` INTEGER, FOREIGN KEY(`organisationID`) REFERENCES `Organisation`(`ID`) )');
+	$ps->execute();
+	$ps = $connection->prepare('CREATE TABLE IF NOT EXISTS Organisation ( ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, address1 TEXT, address2 TEXT, city TEXT, county TEXT, postcode TEXT, email TEXT, phone TEXT)');
+	$ps->execute();
 ?>
